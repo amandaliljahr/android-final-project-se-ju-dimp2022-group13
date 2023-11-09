ef8d80caba0b7d6a71b6863ce3bcf7ffdcda2bf3
amandaliljahr <amandaliljahr@gmail.com>
2023-11-09 10:43:34 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "se.ju.dimp2022.group13"
+include(":app")
